39f7a8675bef3b3a1fd0b99706a4ae8bc4352cd9
Lukas Heumos <lukas.heumos@gmail.com>
2019-03-15 14:15:29 +0100

    [FEATURE][README] Logging

--- spark-submit/local_python_pi.sh
+++ spark-submit/local_python_pi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SPARK_MASTER_URL=spark://spark-master:7077
 SPARK_APPLICATION_PATH=/spark/examples/src/main/python/pi.py
+
 docker run --rm -t --network spark-benchmark_spark-network qbic/spark:latest /spark/bin/spark-submit --master $SPARK_MASTER_URL $SPARK_APPLICATION_PATH 1000
