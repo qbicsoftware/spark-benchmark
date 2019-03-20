3ae3768c6df8b9c170bfa35bf8c1775c5a77b15c
Lukas Heumos <lukas.heumos@gmail.com>
2019-03-20 12:55:49 +0100

    [FEATURE] Added more bash scripts for job launching

--- spark-submit/local_python_pi.sh
+++ spark-submit/local_python_pi.sh
@@ -2,6 +2,7 @@
 SPARK_MASTER_URL=spark://spark-master:7077
 SPARK_APPLICATION_PATH=/spark/examples/src/main/python/pi.py
 
-docker run --rm -t --network spark-benchmark_spark-network qbic/spark:latest_submit /spark/bin/spark-submit --master $SPARK_MASTER_URL $SPARK_APPLICATION_PATH 1000
+docker run --rm -t --network spark-benchmark_spark-network qbic/spark:latest_submit \
+/spark/bin/spark-submit --master $SPARK_MASTER_URL $SPARK_APPLICATION_PATH 1000
 
 
---
+++ spark-submit/mounted_python_pi.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+SPARK_MASTER_URL=spark://spark-master:7077
+SPARK_APPLICATION_PATH=/opt/spark-apps/pi.py
+
+target=/mnt/spark-apps/
+# folder non-empty?
+if [ "$(find "$target" -mindepth 1 -print -quit 2>/dev/null)" ]; then
+    docker run --rm -t --network spark-benchmark_spark-network -v /mnt/spark-apps:/opt/spark-apps qbic/spark:latest_submit \
+    spark/bin/spark-submit --master $SPARK_MASTER_URL $SPARK_APPLICATION_PATH 1000
+else
+    echo "Target '$target' is empty or not a directory"
+fi
+
+
+
