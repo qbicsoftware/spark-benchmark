839745cc9b8409209ef9d4e46266c633ccf9e0ba
Lukas Heumos <lukas.heumos@gmail.com>
2019-08-29 15:47:28 +0200

    [FIX] submit docker image name in submit scripts

--- spark-submit/local_python_pi.sh
+++ spark-submit/local_python_pi.sh
@@ -2,7 +2,7 @@
 SPARK_MASTER_URL=spark://spark-master:7077
 SPARK_APPLICATION_PATH=/spark/examples/src/main/python/pi.py
 
-docker run --rm -t --network spark-service_spark-network qbic/spark:latest_submit \
+docker run --rm -t --network spark-service_spark-network zethson/qbic_spark_submit:latest \
 /spark/bin/spark-submit --master $SPARK_MASTER_URL $SPARK_APPLICATION_PATH 1000
 
 
--- spark-submit/mounted_python_pi.sh
+++ spark-submit/mounted_python_pi.sh
@@ -5,7 +5,7 @@ SPARK_APPLICATION_PATH=/opt/spark-apps/pi.py
 target=/mnt/spark-apps/
 # folder non-empty?
 if [ "$(find "$target" -mindepth 1 -print -quit 2>/dev/null)" ]; then
-    docker run --rm -t --network spark-service_spark-network -v /mnt/spark-apps:/opt/spark-apps qbic/spark:latest_submit \
+    docker run --rm -t --network spark-service_spark-network -v /mnt/spark-apps:/opt/spark-apps zethson/qbic_spark_submit:latest \
     spark/bin/spark-submit --master $SPARK_MASTER_URL $SPARK_APPLICATION_PATH 1000
 else
     echo "Target '$target' is empty or not a directory"
